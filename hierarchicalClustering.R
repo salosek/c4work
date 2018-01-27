03e38bfebb42e32af0b18b8c7c08a0e20c80adae
salosek <salosek@nppd.com>
2018-01-27 09:56:41 -0600

    20180127

--- hierarchicalClustering.R
+++ hierarchicalClustering.R
@@ -8,14 +8,27 @@ save(mt, file="mtcars.Rda")
 dist(dataFrame)
 
 
-hc <- hclust(distxy)
-plot(hc)
-plot(as.dendrogram(hc))
-abline(h=1.5, col="blue")
-abline(h=0.4, col="red")
-abline(h=0.05, col="green")
 
 # http://sebastianraschka.com/Articles/heatmaps_in_r.html#clustering
 
 heatmap()
 heatmap(dataMatrix, col=cm.colors(25))
+
+load(file = "dataFrame.rda")
+
+distxy <- dist(dataFrame)
+hc <- hclust(distxy)
+plot(hc)
+#plot(as.dendrogram(hc))
+abline(h=1.5, col="blue")
+abline(h=0.4, col="red")
+abline(h=0.05, col="green")
+
+
+
+# k-means clustering
+kmeans(dataFrame, centers=3)
+plot(x, y, col=kmObj$cluster, pch=19, cex=2)
+points(kmObj$centers, col=c("black","red","green"), pch=3, cex=3, lwd=3)
+
+plot(x, y, col=kmeans(dataFrame,6)$cluster, pch=19, cex=2)
